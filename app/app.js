1f2bcb05201507f0ebd86eee0e6727e18f1caf8f
Aaira Alias <aaairaalias@gmail.com>
2024-02-26 23:50:22 +0800

    Add initial bot toggle check

--- app/app.js
+++ app/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 
+const fs = require("fs");
+const path = require("path");
+
+
 const app = express();
+
+
+const DATA_FOLDER_NAME = 'data';
 
 
 app.get("/bot/hello", (req, res) => {
@@ -9,5 +16,20 @@ app.get("/bot/hello", (req, res) => {
   })
 });
 
+app.get("/bot/toggle", (req, res) => {
+  const toggleFilePath = path.join(__dirname, DATA_FOLDER_NAME, 'toggle_enabled');
+
+  let response = {"status": "undefined"};
+
+  if (fs.existsSync(toggleFilePath)) {
+    response["status"] = "enabled";
+  }
+  else {
+    response["status"] = "disabled";
+  }
+
+  res.status(200).json(response);
+
+});
 
 module.exports = app;
--- tests/integration/app.test.js
+++ tests/integration/app.test.js
@@ -2,6 +2,9 @@
 const expect = require("chai").expect;
 const request = require("supertest");
 const sinon = require("sinon");
+const fs = require("fs");
+const path = require("path");
+
 const app = require("../../app/app");
 
 describe('GET /bot/hello', () => {
@@ -22,5 +25,66 @@ describe('GET /bot/hello', () => {
         expect(response.headers['content-type']).to.include('application/json');
       })
   });
+});
+
+
+describe('GET /bot/toggle', () => {
+
+  const basePath = path.join(__dirname, '..', '..', 'app', 'data');
+  const fileToCleanup1 = path.join(basePath, 'toggle_enabled');
+  const fileToCleanup2 = path.join(basePath, 'toggle_disabled');
+  
+  beforeEach(() => {
+    if (fs.existsSync(fileToCleanup1)) {
+      fs.unlinkSync(fileToCleanup1);
+    }
+
+    if (fs.existsSync(fileToCleanup2)) {
+      fs.unlinkSync(fileToCleanup2);
+    }    
+  });
+
+  afterEach(() => {
+    if (fs.existsSync(fileToCleanup1)) {
+      fs.unlinkSync(fileToCleanup1);
+    }
+
+    if (fs.existsSync(fileToCleanup2)) {
+      fs.unlinkSync(fileToCleanup2);
+    }    
+  });
+
+  it('should return 200 status', () => {
+    return request(app)
+      .get('/bot/toggle')
+      .then((response) => {
+        expect(response.status).to.eql(200)
+      })
+  });
+
+  it('should return bot enabled status if there is toggle_enabled file', () => {
+
+    fs.writeFileSync(fileToCleanup1, "this is file content");
+
+    return request(app)
+      .get('/bot/toggle')
+      .then((response) => {
+        expect(response.body).to.eql({"status": "enabled"});
+        expect(response.headers['content-type']).to.include('application/json');
+      })
+  });
+
+  it('should return bot disabled status if there is toggle_disabled file', () => {
+
+    fs.writeFileSync(fileToCleanup2, "this is file content");
+
+    return request(app)
+      .get('/bot/toggle')
+      .then((response) => {
+        expect(response.body).to.eql({"status": "disabled"});
+        expect(response.headers['content-type']).to.include('application/json');
+      })
+  });
 
 });
+
